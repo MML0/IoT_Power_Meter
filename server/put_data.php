640583e058d037e25595a286d856cb13eb83e1ac
MML0 <h1337124lat@gmail.com>
2025-06-02 15:18:23 +0430

    Initial commit

---
+++ server/put_data.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = require 'config.php';
+
+$host = $config['database']['host'];
+$dbname = $config['database']['dbname'];
+$username = $config['database']['username'];
+$password = $config['database']['password'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
